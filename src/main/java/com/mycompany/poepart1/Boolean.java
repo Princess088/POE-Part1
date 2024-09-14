356ec4c4301b1feec54234d3343b3700b647cbf0
RC_Student_lab <RC_Student_lab@10.0.0.44>
2024-09-14 12:03:07 +0000

    First commit

---
+++ src/main/java/com/mycompany/poepart1/Boolean.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poepart1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Boolean {
+    
+    public boolean Usernamecheck(){
+        String username = null;
+       
+    System.out.println("Username: " + username);
+    System.out.println("Contains underscore: " + username.contains("_"));
+    System.out.println();
+        return false;
+}
+}
