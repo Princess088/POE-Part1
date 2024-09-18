2fdd8f0e2fb0f9faa73d2d859d7472e066bca6f2
RC_Student_lab <RC_Student_lab@10.0.0.44>
2024-09-18 17:57:36 +0000

    Third commit

--- src/main/java/com/mycompany/poepart1/Main.java
+++ src/main/java/com/mycompany/poepart1/Main.java
@@ -11,20 +11,32 @@ import java.util.Scanner;
  */
         public class Main {
     public static void main(String[] args) {
+         
+        //Create an object for scanner
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
+        
+  System.out.print("Enter username: ");
+        String username = scanner.next();
 
         System.out.print("Enter password: ");
-        String password = scanner.nextLine();
+        String password = scanner.next();
 
         System.out.print("Enter first name: ");
-        String firstName = scanner.nextLine();
+        String firstName = scanner.next();
 
         System.out.print("Enter last name: ");
-        String lastName = scanner.nextLine();
+        String lastName = scanner.next();
 
+        System.out.println(login.registerUser());
+
+        System.out.print("Enter username to login: ");
+        String loginUsername = scanner.next();
+
+        System.out.print("Enter password to login: ");
+        String loginPassword = scanner.next();
+
+        boolean loginStatus = login.loginUser(loginUsername, loginPassword);
+        System.out.println(login.returnLoginStatus(loginStatus));
     }
         }
       
